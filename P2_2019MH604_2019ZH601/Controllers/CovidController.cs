87fee55c61c1a928dbf87a5379bdb58f05b390a7
NestorZepeda <54591161+NestorZepeda@users.noreply.github.com>
2022-04-30 14:01:16 -0600

    Modificacion de Home controller

---
+++ P2_2019MH604_2019ZH601/Controllers/CovidController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P2_2019MH604_2019ZH601.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2_2019MH604_2019ZH601.Controllers
+{
+    public class CovidController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
